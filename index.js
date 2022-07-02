09a22f4c1db57ca3b43680f1c3dc42e1b7e20c3e
Philippe Rivière <fil@rezo.net>
2022-07-02 15:46:33 +0200

    add review version

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import * as d3_1 from "a-173202";
 import * as d3_2 from "a-65942a";
+import * as d3_3 from "a-9693fa";
 
 const d3 = d3_1; // for ancillary functions
 
 const tests = new Map([
   ["173202 (sliced copy)", d3_1],
   ["65942a (circular buffer)", d3_2],
+  ["9693fa (review version)", d3_3],
 ]);
 
 const total = Object.fromEntries(Array.from(tests, ([name]) => [name, 0]));
@@ -44,7 +46,10 @@ console.log("\n= 2d medium");
 for (const [name, b] of tests) {
   const medium = new Float32Array(1e5).map((_, i) => Math.sin(i));
   const t0 = performance.now();
-  for (let i = 0; i < 100; ++i) b.blur2(medium, 100, 1);
+  for (let i = 0; i < 10; ++i)
+    b === d3_3
+      ? b.blur2({ data: medium, width: 100, height: medium.length / 100 }, 1)
+      : b.blur2(medium, 100, 1);
   const t = performance.now() - t0;
   total[name] += t;
   console.log(`${name}: ${t.toFixed(1)}ms`);
@@ -54,7 +59,10 @@ console.log("\n= 2d large");
 for (const [name, b] of tests) {
   const large = new Float64Array(1e6).map((_, i) => Math.sin(i));
   const t0 = performance.now();
-  for (let i = 0; i < 10; ++i) b.blur2(large, 100, 1);
+  for (let i = 0; i < 10; ++i)
+    b === d3_3
+      ? b.blur2({ data: large, width: 100, height: large.length / 100 }, 1)
+      : b.blur2(large, 100, 1);
   const t = performance.now() - t0;
   total[name] += t;
   console.log(`${name}: ${t.toFixed(1)}ms`);
@@ -64,7 +72,9 @@ console.log("\n= 2d huge");
 for (const [name, b] of tests) {
   const huge = new Float64Array(1e7).map((_, i) => Math.sin(i));
   const t0 = performance.now();
-  b.blur2(huge, 100, 1);
+  b === d3_3
+    ? b.blur2({ data: huge, width: 100, height: huge.length / 100 }, 1)
+    : b.blur2(huge, 100, 1);
   const t = performance.now() - t0;
   total[name] += t;
   console.log(`${name}: ${t.toFixed(1)}ms`);
